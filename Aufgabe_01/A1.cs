c1eec2438a1530bf5588a98ca884dc1981db17cc
galinarudollf <49473764+galinarudollf@users.noreply.github.com>
2020-05-06 11:49:08 +0200

    plus erste Aufgabe

---
+++ Aufgabe_01/A1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+namespace Loes02
+{
+        class A1
+        {
+            static void Main(string[] args)
+            {
+                     int minuten;
+                     double gebühr;
+                     char tarif;
+                     try
+                     {
+                         minuten = Int32.Parse(Console.ReadLine());
+                         if (minuten <= 100)
+                             gebühr = 3.99;
+                         else gebühr = 3.99 + (minuten - 100) * 9.9/100;
+                         //b
+                         tarif = Char.Parse(Console.ReadLine());
+                         switch(tarif)
+                         {
+                             case 'S': gebühr = gebühr + 6;break;
+                             case 'M': gebühr = gebühr + 11; break;
+                             case 'L': gebühr = gebühr + 20; break;
+                         }
+                         Console.WriteLine(minuten+" "+tarif+" "+gebühr);
+                     }
+                     catch (FormatException)
+                     {
+                         Console.WriteLine("Falsches Format");
+                     }
+                     catch (OverflowException)
+                     {
+                         Console.WriteLine("Überschreitung des Wertebereichs");
+                     }
+                     catch (Exception)
+                     {
+                         Console.WriteLine("Fehler");
+                     }
+             
+
+            }
+        }
+}
